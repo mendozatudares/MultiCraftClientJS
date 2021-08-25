22df9b71fa173d2c9e01500f69c770e93ff2f0eb
Kevin Mendoza Tudares <mendozatudares@u.northwestern.edu>
2021-08-25 10:55:26 -0500

    define separate handler function for eye direction

--- src/components/EyeTrackingFeed.js
+++ src/components/EyeTrackingFeed.js
@@ -1,11 +1,26 @@
 import React, { useEffect } from "react";
 import { startVideo, trackEyes, stopVideo } from "../utils/eye-tracking";
 
+function handleEyeDirection(direction) {
+  console.log(direction);
+}
+
 function EyeTrackingFeed() {
   useEffect(() => {
     const video = document.getElementById("eye-tracker-feed");
-    startVideo(video).then((model) => setInterval(() => trackEyes(model, video).then((r) => console.log(r)), 500));
-    // return () => stopVideo(video);
+    var interval;
+
+    const initializeWebcam = async () => {
+      const model = await startVideo(video);
+      interval = setInterval(() => trackEyes(model, video).then((direction) => handleEyeDirection(direction)), 500);
+    }
+
+    initializeWebcam();
+
+    return () => {
+      clearInterval(interval);
+      stopVideo(video);
+    }
   }, []);
   return (
     <div>
